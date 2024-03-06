bba04dbcc942a96817c1892acb8f67c68881fbe8
Yusuf <201703768@post.au.dk>
2024-03-06 21:36:41 +0100

    Initial commit, Index, flask and app.py setup

---
+++ app.py
@@ -0,0 +1,16 @@
+"""This is the main file for the application."""
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+
+app = Flask(__name__)
+
+app.secret_key = 'Netcompany123'
+app.config['SECRET_KEY'] = 'Netcompany123'
+
+
+@app.route('/')
+def main():
+    """Main route for the application"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
