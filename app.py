b2d70375c2fff872daf6d68d79600fbe8e92a9a4
Yusuf <201703768@post.au.dk>
2024-03-06 21:58:56 +0100

    Update README and app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 """This is the main file for the application."""
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 app.secret_key = 'Netcompany123'
